c48883db9d202a1858ea7cb7e0a4be758a7cfd79
betasunk <979991000@qq.com>
2024-04-23 16:30:43 +0800

    feat: x项目初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import blogComponent from '@/components/blogComponent/blog-index.vue'
+import { ref } from 'vue'
+</script>
+
+<template>
+  <main>
+    <blogComponent></blogComponent>
+  </main>
+</template>
